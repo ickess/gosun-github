70f645edd553eb9797256a3bb13d80528b87a143
xialiang <eksliang@163.com>
2015-10-19 17:22:39 +0800

    添加一个for循环

--- src/main/java/com/gosun/github/gosun_github/GitHub.java
+++ src/main/java/com/gosun/github/gosun_github/GitHub.java
@@ -3,7 +3,11 @@ package com.gosun.github.gosun_github;
 public class GitHub {
 	public static void main(String[] args) {
 		System.out.println("GitHub!");
-		System.out.println("github!");
-		System.out.println("亮帅是谁?是夏亮.");
+		System.out.println("githubddd!");
+		System.out.println("亮帅是谁?是夏dfds亮.");
+		int i=1;
+		for (String string : args) {
+			System.out.println(string);
+		}
 	}
 }
